c2b751edf9c535fd240c59bebb6a9a357c7d0d9f
lmn31190 <monielouis31@gmail.com>
2024-02-27 23:43:14 +0100

    portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,85 @@
+import { useRef } from "react";
 import "./portfolio.scss";
 
-const Portfolio = () => {
-    return (
-        <div className="portfolio">
-            
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+
+const items = [
+  {
+    id: 1,
+    title: "Next Ecommerce",
+    img: "/next-ecommerce.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: 2,
+    title: "React Ecommerce",
+    img: "/react-ecommerce.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: 3,
+    title: "The restaut",
+    img: "/restaut.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: 4,
+    title: "La montagne",
+    img: "/mountain.png",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+];
+
+const Single = ({ item }) => {
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    // offset: ["start start", "end start"]
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+  return (
+    <section >
+      <div className="container">
+        <div className="wrapper">
+          <div className="imgContainer" ref={ref}>
+            <img src={item.img} alt="" />
+          </div>
+
+          <motion.div className="textContainer" style={{y}}>
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <button>Voir démo</button>
+          </motion.div>
         </div>
-    );
+      </div>
+    </section>
+  );
+};
+
+const Portfolio = () => {
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  });
+
+  return (
+    <div className="portfolio" ref={ref}>
+      <div className="progress">
+        <h1>Mes réalisations</h1>
+        <motion.div style={{ scaleX }} className="progressBar"></motion.div>
+      </div>
+      {items.map((item) => (
+        <Single item={item} key={item.id} />
+      ))}
+    </div>
+  );
 };
 
 export default Portfolio;
